ef8d77420fd5fd135047d80e70ba511dd98a7333
worldgu <15893851179@163.com>
2021-04-02 17:43:22 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "C.exe"
+  "C.exe.manifest"
+  "C.lib"
+  "C.pdb"
+  "CMakeFiles/C.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    int num;
+    num = 1;
+    printf("I am a simple ");
+    printf("coputer . \n");
+    printf("My Favorite number is %d because it is first . \n ,num");
+    return 0;
+}
